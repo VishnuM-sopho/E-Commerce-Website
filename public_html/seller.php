baa8957605a740215f09371906bf2e57ba0fcf33
jharvard <sakshamagarwal1997@gmail.com>
2017-03-25 09:05:40 -0400

    contact seller done

--- public_html/seller.php
+++ public_html/seller.php
@@ -3,11 +3,48 @@
 <body>
 <a href="../models/login.php">Sell Item</a>
 <a href="/public_html/store.php">Go to Store</a>
-<a href=" ">View other items from this Seller</a>
+<a href=" ">View other items from this Seller</a><br>
 </body>
 </html>
 <?php
 require("../controllers/config.php");
 $_SESSION["id"]=$_GET["id"];
 $id=$_SESSION["id"];
+$title=$_GET["title"];
+$query="Select * from store where seller_id=$id and title='$title'";
+$result=mysqli_query($conn,$query);
+    if(!$result)
+    print(mysqli_error($conn));
+$rows=mysqli_fetch_assoc($result);
+echo("<table id=\"store\">");
+echo("<tr>");
+echo("<th>Image</th>");
+echo("<th>Title</th>");
+echo("<th>Description</th>");
+echo("<th>Price</th>");
+echo("<th>Date</th>");
+echo("<th>Contact</th>");
+echo("</tr>");
+$id=$rows["seller_id"];
+	print("<tr>");
+	$image="<img src=\"".$rows["image"]."\">";
+    print("<td>".$image."</td>");
+    print("<td>" . $rows["title"] . "</td>");
+    $query="select description,contact from item_desc where title='$title'";
+    $res=mysqli_query($conn,$query);
+    $row=mysqli_fetch_assoc($res);
+    $desc=$row["description"];
+    $contact=$row["contact"];
+
+
+
+
+
+
+    print("<td>" . $desc . "</td>");
+    print("<td>" . $rows["price"] . "</td>");                                            
+    print("<td>" . $rows["date"] . "</td>");
+    print("<td>".$contact."</td>");
+    print("</tr>");
+    echo("</table>");     
 ?>
