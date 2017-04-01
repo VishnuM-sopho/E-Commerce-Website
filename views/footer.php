9dcb04711537fb27e2d8616c116de755a60c667d
jharvard <sakshamagarwal1997@gmail.com>
2017-04-01 05:15:38 -0400

    footer improved

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
             <br><br><br>
             <footer><!-- Defining the footer section of the page -->
-<div id="privacy">
-OLX CLONE © 2017 <a class="link" href="">Privacy Policy</a><br/>
+<div id="privacy" align="center">
+OLX CLONE &reg; 2017 <a class="link" href="">Privacy Policy</a><br/>
 <a class="link" href="https://www.sophomores.in">By Sophomores.in</a>
 </div>
 </footer>
