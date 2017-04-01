e0f96952002c1085b3200ef03248569127672049
Vishnu Mv <vishnum1998@gmail.com>
2017-04-01 21:33:05 +0000

    added the search function and removed the category and college dropdown

--- public_html/store.php
+++ public_html/store.php
@@ -39,81 +39,54 @@ nav {
   padding: 6px 0; 
 }
 
+.field {
+  display:flex;
+  position:realtive;
+  margin:5em auto;
+  width:80%;
+}
+
+.field>input[type=text],
+.field>button {
+  display:block;
+  font:1.2em Myriad Pro;
+}
+
+.field>input[type=text] {
+  flex:1;
+  padding:0.6em;
+  border:0.2em ;
+}
+
+.field>button {
+  padding:0.6em 0.8em;
+  background-color:#2ecc71;
+  color:white;
+  border:none;
+}
+.field>button:hover {
+	cursor:pointer;
+}
+
         </style>
         
-     
+  <script>
+$("#search").keyup(function () {
+    var filter = jQuery(this).val();
+    jQuery("section div ul li").each(function () {
+        if (jQuery(this).text().search(new RegExp(filter, "i")) < 0) {
+            jQuery(this).hide();
+        } else {
+            jQuery(this).show()
+        }
+    });
+});</script>   
 </body>
 </html>
 <?php
-if(isset($_POST["cid"]))
-    extract($_POST);
-
-if(isset($_SESSION["id"]) && isset($_POST["cid"]))
-{   
-    $id=$_SESSION["id"];
-    $query="select * from store where not seller_id=$id";
-    if($cid==0 && $catid==0)
-      {  $query="select * from store  where not seller_id=$id";
-        $res="All Colleges";
-        $res2="All Categories";
-        }
-    else if($cid!=0 && $catid!=0)
-        $query=$query." and college_id=$cid and category_id=$catid";
-    else if($cid!=0)
-       { $query=$query." and college_id=$cid";$res2="All Categories";}
-    else if($catid!=0)
-       { $query=$query." and category_id=$catid";$res="All Colleges";}
-    if(!mysqli_query($conn,$query))
-        print(mysqli_error($conn));
-}
-else if(isset($_SESSION["id"]) && !isset($_POST["cid"]))
-{
-    $id=$_SESSION["id"];
-    $query="select * from store where not seller_id=$id";
+$query="Select * from store";
     $res="All Colleges";
     $res2="All Categories";
-    if(!mysqli_query($conn,$query))
-        print(mysqli_error($conn));
-}
-else if(!isset($_SESSION["id"]) && isset($_POST["cid"]))
-{
-    $query="select * from store";
-    if($cid==0 && $catid==0)
-       { $query="select * from store";$res="All Colleges";$res2="All Categories";}
-    else if($cid!=0 && $catid!=0)
-        $query=$query." where college_id=$cid and category_id=$catid";
-    else if($cid!=0)
-       { $query=$query." where college_id=$cid";$res2="All Categories";}
-    else if($catid!=0)
-       { $query=$query." where category_id=$catid";$res="All Colleges";}
-    if(!mysqli_query($conn,$query))
-        print(mysqli_error($conn));
-}
-else if(!isset($_SESSION["id"]) && !isset($_POST["cid"]))
-{
-    $query="Select * from store";
-    $res="All Colleges";
-    $res2="All Categories";
-}
-if(isset($_POST["cid"]))
-{
-    if($_POST["cid"]!=0){
-$id=$_POST['cid'];
-$query2="Select college_name from colleges where college_id=$id";
-$r=mysqli_query($conn,$query2);
-$res=mysqli_fetch_assoc($r);
-$res=$res["college_name"];
-}
-else $res="All Colleges";
-if($_POST["catid"]!=0){
-$query3="select category from categories where category_id=$catid";
-$r=mysqli_query($conn,$query3);
-$res2=mysqli_fetch_assoc($r);
-$res2=$res2["category"];}
-else $res2="All Categories";
-}
-print("<h4>Showing Results for $res and $res2</h4>");
-
     $result=mysqli_query($conn,$query);
     if(!$result)
     print(mysqli_error($conn));
@@ -122,37 +95,6 @@ print("<h4>Showing Results for $res and $res2</h4>");
  
     
 ?>
-<?php
- if(!isset($_GET["id"]))
-echo("<form id=\"search\" method=\"POST\" action=\"../public_html/store.php\">");
-else
-echo("<form id=\"search\" method=\"POST\" action=\"../public_html/store.php?id=\"".$_GET["id"].">");
-?>
-<select name='cid' id="s1">
-<option value=0 selected>All Colleges</option>
-<option value=2>MNIT, JAIPUR</option>
-<option value=3>NIT JALANDHAR</option>
-<option value=4>IIT BOMBAY</option>
-<option value=5>IIT DELHI</option>
-<option value=6>IIT KHARAGPUR</option>
-<option value=7>IIT KANPUR</option>
-<option value=8>IIT MADRAS</option>
-<option value=9>IIT GUWAHATI</option>
-<option value=10>IIT ROORKEE</option>
-<option value=11>IIT (BHU) VARANASI</option>
-			</select><br>
-<select name="catid" id="s2">
-        <option value=0 selected>All Categories</option>
-        <option value="1">Books</option>
-        <option value="2">Clothing</option>
-        <option value="3">Electronics</option>
-        <option value="4">Furniture</option>
-        <option value="5">Sports</option>
-        <option value="6">Vehicle</option>
-        <option value="7">Others</option>
-    </select><br>
-			<button type="submit" id="b1" form="search" name="submit" value="Register">Submit</button>
-</form>
 
 <?php
     if(count($rows)==0)
@@ -160,6 +102,10 @@ echo("<form id=\"search\" method=\"POST\" action=\"../public_html/store.php?id=\
     else
     {$l=mysqli_num_rows($res);
     print("<h2>".$l." Items on Sale Available for You</h2>");
+    print("<div class=\"field\" id=\"searchform\">
+		      <input type=\"text\" id=\"search\" placeholder=\"Search anything ?\" />
+		      <button type=\"button\" id=\"search\">Find</button>
+  	  </div>");
     echo("<div id=\"main\"><section id=\"content\"><div id=\"left\"><ul>");
 while($rows=mysqli_fetch_assoc($result))
  {   
@@ -200,3 +146,14 @@ while($rows=mysqli_fetch_assoc($result))
         require("../views/footer.php");    
 
 ?>
+<script>
+$("#search").keyup(function () {
+    var filter = jQuery(this).val();
+    jQuery("section div ul li").each(function () {
+        if (jQuery(this).text().search(new RegExp(filter, "i")) < 0) {
+            jQuery(this).hide();
+        } else {
+            jQuery(this).show()
+        }
+    });
+});</script>   
